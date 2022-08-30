2d2192c40890f710b139460774e30b7c57a6e5e4
Eduard Schander <66794307+EddeCCC@users.noreply.github.com>
2022-08-30 15:04:26 +0200

    Interactive Cards (#1)
    * Prortype Car and Train moving
    
    * Implemented the Car states and movements
    
    The car has multiple states, depending on the input of the user, creating different conditions for it to start or stop moving.
    
    * Main Car Functionality Done
    
    * Success Area Reached works
    
    Co-authored-by: Dohruba <71273811+Dohruba@users.noreply.github.com>

--- DummyEduGame/Assets/Scripts/Car.cs
+++ DummyEduGame/Assets/Scripts/Car.cs
@@ -1,38 +1,164 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public enum MovementType
 {
-    time, velocity
+    time, velocity, distance, finished
 }
 
 public class Car : MonoBehaviour
 {
-
     [SerializeField]
     private float speed;
     [SerializeField]
     private Vector3 position;
 
-    private float distance;
-    private float time;
-    private float velocity;
-    private MovementType state;
+    [SerializeField]
+    private float distance = 0;
+    [SerializeField]
+    private float time = 0;
+    [SerializeField]
+    private float velocity = 0;
+    private float defaultVelocity = 5;
 
+    [SerializeField]
+    private MovementType state = MovementType.finished;
+    private bool isTimerCounting = false;
+    private bool isMovingToDistance = false;
+
+    private IEnumerator timerCorutine;
+    private IEnumerator distanceCorutine;
+
+
+    private void Awake()
+    {
+        position = transform.position;
+        timerCorutine = StartTimer();
+        distanceCorutine = WaitForDistance();
+    }
     public void Go(float distance, float time, float velocity)
     {
-        if (distance == 0) 
-            this.distance = 15;
-        if (time == 0) 
-            this.time = 10;
-        if (velocity == 0) 
-            this.velocity = 5;
+        this.distance = distance != 0 ? distance : this.distance;
+        this.time = time != 0 ? time : this.time;
+        this.velocity = velocity != 0 ? velocity : this.velocity;
 
+        this.state = SetState(this.distance, this.time, this.velocity);
+    }
+    private MovementType SetState(float distance, float time, float velocity)
+    {
+        if(distance != 0 && time != 0)
+        {
+            speed = distance / time;
+            return MovementType.velocity;
+        }
+        if (velocity != 0)
+        {
+            speed = velocity;
+            return MovementType.velocity;
+        }
+        if(distance != 0)
+        {
+            speed = defaultVelocity;
+            return MovementType.distance;
+        }
+        if(time != 0)
+        {
+            speed = defaultVelocity;
+            return MovementType.time;
+        }
+        return MovementType.finished;
+    }
+
+    private void Update()
+    {
+        if(state != MovementType.finished)
+        {
+            StartMovement();
+        }
+    }
+
+    private void StartMovement()
+    {
         switch (state)
         {
             case MovementType.velocity:
-
+                if(distance == 0 && time == 0)
+                {
+                    VelocityDependantMovement();
+                }
+                else
+                {
+                    TimeAndDistanceDependantMovement();
+                }
+                break;
+            case MovementType.time:
+                TimeDependantMovement();
+                break;
+            case MovementType.distance:
+                DistanceDependantMovement();
+                break;
+            case MovementType.finished:
                 break;
         }
+    }
+
+    private void VelocityDependantMovement()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z + speed * Time.deltaTime;
+
+        transform.position = new Vector3(x, y, z);
+    }
+    private void TimeDependantMovement()
+    {
+        VelocityDependantMovement();
+        if (!isTimerCounting) StartCoroutine(timerCorutine);
+    }
+    private IEnumerator StartTimer()
+    {
+        isTimerCounting = true;
+        yield return new WaitForSecondsRealtime(time);
+        state = MovementType.finished;
+        isTimerCounting = false;
+        Debug.Log("Time Out");
+        if (isMovingToDistance)
+        {
+            StopCoroutine(distanceCorutine);
+            isMovingToDistance = false;
+        }
+    }
+    private void DistanceDependantMovement()
+    {
+        VelocityDependantMovement();
+        if (!isMovingToDistance) StartCoroutine(distanceCorutine);
+    }
+    private void TimeAndDistanceDependantMovement()
+    {
+        VelocityDependantMovement();
+        if (!isTimerCounting) StartCoroutine(timerCorutine);
+    }
+
+    private IEnumerator WaitForDistance()
+    {
+        isMovingToDistance = true;
+        yield return new WaitUntil(() => Vector3.Distance(position, transform.position) > distance - 0.05);
+        state = MovementType.finished;
+        isMovingToDistance = false;
+        Debug.Log("Distance Reached");
+        if (isTimerCounting)
+        {
+            StopCoroutine(timerCorutine);
+            isTimerCounting = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Entered");
+        Debug.Log(state);
+        Debug.Log(other.gameObject.tag);
+        state = MovementType.finished;
+        if (other.gameObject.tag.Equals("SuccessArea")) Debug.Log("Yeeeeeey");
     }
 }
--- DummyEduGame/Assets/Scripts/Train.cs
+++ DummyEduGame/Assets/Scripts/Train.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TrainState
+{
+    running, finished
+}
 public class Train : MonoBehaviour
 {
 
@@ -9,4 +14,30 @@ public class Train : MonoBehaviour
     private float speed;
     [SerializeField]
     private Vector3 position;
+    [SerializeField]
+    private TrainState state = TrainState.finished;
+
+    private void Update()
+    {
+        if (state != TrainState.finished)
+        {
+            speed = 3;
+            float x = gameObject.transform.position.x + speed * Time.deltaTime;
+            float y = gameObject.transform.position.y;
+            float z = gameObject.transform.position.z;
+
+            gameObject.transform.position = new Vector3(x, y, z);
+            if (x > 10) state = TrainState.finished;
+        }
+    }
+
+    internal void Begin()
+    {
+        state = TrainState.running;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Entered");
+        state = TrainState.finished;
+    }
 }
