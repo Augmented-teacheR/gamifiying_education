1826fd081044bf6e87d1c2c8cc3b98b11fbeba13
Eduard Schander <66794307+EddeCCC@users.noreply.github.com>
2022-08-30 15:09:26 +0200

    Interactive_Cards (#2)
    * add ReadValues
    
    * add blocks into Scene
    
    * add UI to cards
    
    * change UI text

--- DummyEduGame/Assets/Scripts/Block.cs
+++ DummyEduGame/Assets/Scripts/Block.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum BlockType
 {
-    speed, distance, time
+    velocity, distance, time
 }
 public class Block : MonoBehaviour
 {
@@ -12,4 +12,47 @@ public class Block : MonoBehaviour
     private BlockType type;
     [SerializeField]
     private float value;
+
+    private CodeBlock codeBlock;
+
+    // TODO REMOVE LATER - JUST FOR TESTING
+    public string key;
+
+    private void Awake()
+    {
+        this.codeBlock = FindObjectOfType<CodeBlock>();
+    }
+
+    public BlockType GetBlockType()
+    {
+        return this.type;
+    }
+
+    public float GetValue()
+    {
+        return this.value;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(key))
+        {
+            float speed = -15.0f;
+            float newX = transform.position.x + speed * Time.deltaTime;
+            float y = transform.position.y;
+            float z = transform.position.z;
+
+            transform.position = new Vector3(newX, y, z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        codeBlock.AddBlock(this);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        codeBlock.RemoveBlock(this);
+    }
 }
--- DummyEduGame/Assets/Scripts/CodeBlock.cs
+++ DummyEduGame/Assets/Scripts/CodeBlock.cs
@@ -6,5 +6,54 @@ public class CodeBlock : MonoBehaviour
 {
     [SerializeField]
     private List<Block> blocks;
-    
+    [SerializeField]
+    private float readDistance;
+    [SerializeField]
+    private float readTime;
+    [SerializeField]
+    private float readVelocity;
+
+    public void ReadValues()
+    {        
+        foreach(Block block in blocks) 
+        {
+            switch (block.GetBlockType()) 
+            {
+                case BlockType.distance:
+                    this.readDistance = block.GetValue();
+                    break;
+                case BlockType.time:
+                    this.readTime = block.GetValue();
+                    break;
+                case BlockType.velocity:
+                    this.readVelocity = block.GetValue();
+                    break;
+            }
+        }
+    }
+
+    public void AddBlock(Block block)
+    {
+        blocks.Add(block);
+    }
+
+    public void RemoveBlock(Block block)
+    {
+        blocks.Remove(block);
+    }
+
+    public float GetDistance()
+    {
+        return this.readDistance;
+    }
+
+    public float GetTime()
+    {
+        return this.readTime;
+    }
+
+    public float GetVelocity()
+    {
+        return this.readVelocity;
+    }
 }
